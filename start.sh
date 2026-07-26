47f79e0b656a49972455b0b02482fa07791999a0
VACKKK CoDeBoY <92257233+zVACK@users.noreply.github.com>
2026-07-26 17:25:11 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,10 +10,5 @@ chmod 1777 /tmp/.X11-unix
 # Start PipeWire PulseAudio service in background
 pipewire-pulse &
 
-# Start xRDP service
-service xrdp start
-service xrdp-sesman start
-
-# Keep container running by tailing log files
-touch /var/log/xrdp-sesman.log /var/log/xrdp.log
-tail -f /var/log/xrdp*.log
+# Keep container running continuously
+exec tail -f /dev/null
