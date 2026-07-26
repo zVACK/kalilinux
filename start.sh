b7aa36463414063ff46103366d93b659d322f9b1
VACKKK CoDeBoY <92257233+zVACK@users.noreply.github.com>
2026-07-26 18:17:21 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# دروستکردنی فۆڵدەری X11 پێش دەستپێکردنی xrdp
+# دروستکردنی فۆڵدەری X11 پێش دەستپێکردنی xrdp بۆ ڕێگری لە کێشەی Connection Log
 mkdir -p /tmp/.X11-unix
 chmod 1777 /tmp/.X11-unix
 rm -rf /tmp/.X*-lock /tmp/.X11-unix/X*
@@ -10,6 +10,6 @@ service dbus start
 service xrdp start
 service xrdp-sesman start
 
-# هێشتنەوەی کانتێنەرەکە
+# هێشتنەوەی کانتێنەرەکە بە بەردەوامی
 touch /var/log/xrdp-sesman.log
 tail -f /var/log/xrdp-sesman.log
