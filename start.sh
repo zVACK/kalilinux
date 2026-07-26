55f51282d6c69992b185311639030838a3ae7bc7
VACKKK CoDeBoY <92257233+zVACK@users.noreply.github.com>
2026-07-26 16:35:29 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+# Start DBus service
 service dbus start
-pulseaudio --start --system --disallow-exit --disable-shm
-service xrdp start
 
+# Prepare X11 socket folder
 mkdir -p /tmp/.X11-unix
 chmod 1777 /tmp/.X11-unix
 
-tail -f /var/log/xrdp-sesman.log
+# Start PulseAudio in system mode safely
+pulseaudio --start --system --disallow-exit --disable-shm --exit-idle-time=-1
+
+# Start xRDP service
+service xrdp start
+service xrdp-sesman start
+
+# Keep container running by tailing log files
+touch /var/log/xrdp-sesman.log /var/log/xrdp.log
+tail -f /var/log/xrdp*.log
